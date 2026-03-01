450fa8ccd8bad1b25b3aee7f1a95c1f09d513436
Pranjali B <pranjalibhot@gmail.com>
2026-03-01 23:58:47 +0000

    Add .gitignore and remove IDE-specific files

---
+++ incident-management/src/main/java/com/tus/incidentmanagement/IncidentManagementApplication.java
@@ -0,0 +1,12 @@
+package com.tus.incidentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentManagementApplication.class, args);
+    }
+}
